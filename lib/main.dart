91200042f32872c5abce7dc7279e4d475b1ca13b
Abdulrahman Akram <bdwakrm5@gmail.com>
2024-11-29 07:17:27 +0200

    Add .env file and other updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:grad_project/Screens/auth/login_screen.dart';
 import 'package:grad_project/Screens/auth/register_screen.dart';
 import 'package:grad_project/Screens/set_password.dart';
@@ -11,6 +12,8 @@ import 'Screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(); // Load the .env file
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -32,7 +35,7 @@ class MyApp extends StatelessWidget {
         LoginScreen.routeName: (context) => const LoginScreen(),
         SetPassword.routeName: (context) => const SetPassword(),
       },
-      home:  HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
